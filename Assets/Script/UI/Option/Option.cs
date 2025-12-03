82eca77b7e65fe4890bdea3fd12bef93f7a3c2a5
DESKTOP-TTSUQC3\duqor <duqorghg@gmail.com>
2025-12-03 14:47:48 +0900

    일단 UI 좀 더 만들었고 언어 변경하면 적용되지 않는다고 생각했는데 적용중였던거임

--- Assets/Script/UI/Option/Option.cs
+++ Assets/Script/UI/Option/Option.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 public class Option : MonoBehaviour
 {
     [SerializeField]
+    private Button b_close;
+
+    [SerializeField]
     private Button b_general;
     [SerializeField]
     private Button b_graphics;
@@ -26,6 +29,13 @@ public class Option : MonoBehaviour
 
     private void OnEnable()
     {
+        void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        b_close.onClick.AddListener(Close);
+
         void General()
         {
             p_general.SetActive(true);
@@ -56,20 +66,71 @@ public class Option : MonoBehaviour
         d_language.ClearOptions();
         List<string> values = Enum.GetNames(typeof(Language)).ToList();
         d_language.AddOptions(values);
-        //d_language.value = 0;
+        d_language.value = (int)LocalizationManager.Instance.Current;
         d_language.onValueChanged.AddListener(OnChangeLanguage);
+
+        InitUI();
     }
 
     private void OnDisable()
     {
+        b_close.onClick.RemoveAllListeners();
         b_general.onClick.RemoveAllListeners();
         b_graphics.onClick.RemoveAllListeners();
         b_sound.onClick.RemoveAllListeners();
+        d_language.onValueChanged.RemoveAllListeners();
+        d_resolutions.onValueChanged.RemoveAllListeners();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    [SerializeField]
+    private TMP_Dropdown d_resolutions;
+    [SerializeField]
+    private Toggle t_fullScreen;
+    List<Resolution> resolutions = new();
+
+    private void InitUI()
+    {
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRateRatio.value >= 60)
+            {
+                resolutions.Add(Screen.resolutions[i]);
+            }
+        }
+
+        d_resolutions.options.Clear();
+        List<string> options = new();
+        int value = 0;
+        for(int i = 0; i < resolutions.Count; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                value = i;
+        }
+        d_resolutions.AddOptions(options);
+        d_resolutions.value = value;
+        d_resolutions.RefreshShownValue();
+
+        t_fullScreen.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+
+        d_resolutions.onValueChanged.AddListener(DropdownValueChange);
+    }
+
+    public void DropdownValueChange(int _value)
+    {
+        Apply(_value);
+    }
+
+    private void Apply(int value)
+    {
+        Screen.SetResolution(resolutions[value].width, resolutions[value].height, 
+            t_fullScreen.isOn ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed);
+        //SoundManager.instance.SFXCreate(SoundManager.Clips.ButtonClip, 1, 0);
     }
 }
